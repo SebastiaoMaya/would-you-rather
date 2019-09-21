2d0847b8583c5561cd630c1633395d8cff53ea92
Maya, Sebastiao <sebastiao.maya@sap.com>
2019-09-21 17:26:35 +0100

    chore: added base structure for actions, reducers, components and middleware

---
+++ src/components/question/AnsweredDetails.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AnsweredDetails() {
+  return <div></div>;
+}
---
+++ src/components/question/SimplifiedDetails.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SimplifiedDetails() {
+  return <div></div>;
+}
